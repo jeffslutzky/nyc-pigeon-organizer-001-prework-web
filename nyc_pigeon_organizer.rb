41443fc85ad0f9abf51336c9154d539a8b1a440d
jeffslutzky <jeffslutzky@gmail.com>
2015-09-05 22:42:00 -0400

    done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, values|
+    values.each do |actual_value, birds|
+      birds.each do |name|
+        if !pigeon_list[name]
+          pigeon_list[name] = {}
+        end
+        if !pigeon_list[name][attribute]
+          pigeon_list[name][attribute] = []
+        end
+          pigeon_list[name][attribute] << actual_value.to_s
+      end
+    end
+  end
+  pigeon_list
 end
